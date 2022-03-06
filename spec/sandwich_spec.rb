8a3773879a1feb94d1d1cdb90020b14d80bff766
fi-ya <safiaali@hotmail.com>
2022-03-06 17:56:02 +0000

    refactor: use before hook to create @sandwich instance

--- spec/sandwich_spec.rb
+++ spec/sandwich_spec.rb
@@ -1,20 +1,21 @@
 Sandwich = Struct.new(:taste, :toppings)
 RSpec.describe 'An ideal sandwich' do
     
+    # hook
+    before { @sandwich = Sandwich.new('delicous', [])}
+
     it 'is delcious' do
-        # Arrange/Setup - an object to do something with it
-        sandwich = Sandwich.new('delicous', [])
+        # Arrange/Setup - an object to do something with it - See before hook
         # Act/Exercise - run/call the system(class/object/method) under test (SUT/CUT/OUT/MUT)
-        taste = sandwich.taste
+        taste = @sandwich.taste
         # Assert/Verify - check it behaved the way you wanted 
         expect(taste).to eq('delicous')
         #Annialte/Teardown - feature for next test
     end
 
     it 'lets me add toppings' do
-        sandwich = Sandwich.new('delicious', [])
-        sandwich.toppings << 'cheese'
-        toppings = sandwich.toppings
+        @sandwich.toppings << 'cheese'
+        toppings = @sandwich.toppings
         # negate expecatation ie. check for a falsehood using `not_to`
         expect(toppings).not_to be_empty
     end
